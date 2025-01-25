45a5873f11fe85f0a3cd80007d40837f82687cff
Zumar15 <zumarawesome@gmail.com>
2025-01-25 11:27:06 -0700

    Creating the week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+export function StudentInfo() {
+    return (
+    <>
+    <h2>Zumar Syed Bukhari</h2>
+    <Link href="https://github.com/Zumar15/cprg306-assignments">https://github.com/Zumar15/cprg306-assignments</Link>
+    </>
+    );
+}
